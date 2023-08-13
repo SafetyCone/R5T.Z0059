29efd274c351caa8a9d0938a579137c4f60c1a9e
davidcoats <david@getsafetycone.com>
2023-08-12 23:24:40 -0700

    Interim changes.

--- source/R5T.Z0059/Code/Values/IOrganizationNames.cs
+++ source/R5T.Z0059/Code/Values/IOrganizationNames.cs
@@ -7,6 +7,9 @@ using R5T.T0222.Extensions;
 
 namespace R5T.Z0059
 {
+    /// <summary>
+    /// Top-level organization names.
+    /// </summary>
     [ValuesMarker]
     public partial interface IOrganizationNames : IValuesMarker
     {
@@ -14,17 +17,26 @@ namespace R5T.Z0059
         /// <para>"David Coats"</para>
         /// Note: has a space.
         /// </summary>
-        public IOrganizationName DavidCoats => "David Coats".ToOrganizationName();
+        public const string DavidCoats_Constant = "David Coats";
+
+        /// <inheritdoc cref="DavidCoats_Constant"/>
+        public IOrganizationName DavidCoats => IOrganizationNames.DavidCoats_Constant.ToOrganizationName();
 
         /// <summary>
         /// <para>"Rivet"</para>
         /// </summary>
+        public const string Rivet_Constant = "Rivet";
+
+        /// <inheritdoc cref="Rivet_Constant"/>
         public IOrganizationName Rivet => "Rivet".ToOrganizationName();
 
         /// <summary>
         /// <para>"Safety Cone"</para>
         /// Note: has a space.
         /// </summary>
+        public const string SafetyCone_Constant = "Safety Cone";
+
+        /// <inheritdoc cref="SafetyCone_Constant"/>
         public IOrganizationName SafetyCone => "Safety Cone".ToOrganizationName();
     }
 }
